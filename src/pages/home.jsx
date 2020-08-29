d4c348f692d9ee38d0a0f65dfae54d043a49d243
ali <ali@iMac-de-Elodie.fritz.box>
2020-08-29 23:37:14 +0200

    component search

---
+++ src/pages/home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Search from '../components/Search';
+
+
+const home = () => {
+    return (
+        <div>
+            <Search />
+
+        </div>
+    )
+}
+
+export default home
+
+
+10: 00 dk
